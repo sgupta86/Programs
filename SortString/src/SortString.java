07eeacb02fd85222b04a1095e54a40e5ffea14c7
saurav <saurav@192.168.0.3>
2017-07-01 14:25:33 -0400

    SortString Code

---
+++ SortString/src/SortString.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortString {
+
+	public static void main(String[] args) {
+		
+		List<String> listOfStrings= new ArrayList<String>();
+		for(String s: args)
+			listOfStrings.add(s);
+
+		Collections.sort(listOfStrings);
+		for(int i =0; i <listOfStrings.size();i++){
+			System.out.println(listOfStrings.get(i));
+		}
+
+	}
+
+}
